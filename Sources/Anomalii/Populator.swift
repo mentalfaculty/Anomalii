9f37c902d3e8405276f6be7930ce582501d6ccc5
Drew McCormack <drewmccormack@mac.com>
2017-11-25 15:02:16 +0100

    Changed PopulationInitializer to Populator.

--- Sources/Anomalii/Populator.swift
+++ Sources/Anomalii/Populator.swift
@@ -1,5 +1,5 @@
 //
-//  PopulationInitializer.swift
+//  Populator.swift
 //  Anomalii
 //
 //  Created by Drew McCormack on 14/11/2017.
@@ -7,23 +7,23 @@
 
 import Foundation
 
-struct InitialPopulationConstraints {
-    let populationSize: Int
-    let maximumDepth: Int
-}
-
-class PopulationInitializer {
-    let constraints: InitialPopulationConstraints
+class Populator {
+    struct Constraints {
+        let populationSize: Int
+        let maximumDepth: Int
+    }
+    
+    let constraints: Constraints
     let variables: [Variable]
     private let expressionMaker: ExpressionMaker
     
-    init(withConstaints constraints: InitialPopulationConstraints, variables: [Variable]) {
+    init(withConstaints constraints: Constraints, variables: [Variable]) {
         self.constraints = constraints
         self.variables = variables
         self.expressionMaker = ExpressionMaker(variables: variables, maximumDepth: constraints.maximumDepth)
     }
     
-    func makeInitialPopulation() -> [Expression] {
+    func makePopulation() -> [Expression] {
         var population = [Expression]()
         for _ in 0..<constraints.populationSize {
             let expression = expressionMaker.expression(withOutputType: .scalar)
